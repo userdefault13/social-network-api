8476bae3f02953ad59ee1ed9ca3d6cc784a2d0a9
userdefault13 <user.default@me.com>
2023-08-18 20:23:07 -0700

    worked on models

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model, VirtualType } = require("mongoose");
+// const thoughtSchema = require("./Thought");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,8 @@
+// // Importing the required models
+const User = require("./User");
+const Thought = require("./Thought");
+
+// // Exporting the models
+module.exports = { User, Thought };
+
+
